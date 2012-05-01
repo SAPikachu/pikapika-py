f1203805802b377da7453e7ce0f9e14ac3db90f6
SAPikachu <sapikachu@gmail.com>
2012-05-01 20:27:07 +0800

    Add a script to quickly reset the database

---
+++ reset-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./manage.py dbshell <<EOF
+
+drop database pikapika;
+create database pikapika character set utf8;
+
+EOF
+
+./manage.py syncdb --noinput
+
+./manage.py loaddata fixtures/default_user.json
+
