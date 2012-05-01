c058a36676fc4badb59c30b8532ccb4b52925688
SAPikachu <sapikachu@gmail.com>
2012-05-01 17:27:12 +0800

    Relax model restrictions

--- pikapika/novel/models.py
+++ pikapika/novel/models.py
@@ -4,19 +4,19 @@ from django.contrib.auth.models import User
 IMAGE_DIR = "images/%Y/%m/%d"
 
 class Novel(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=512)
     description = models.TextField()
     publisher = models.CharField(max_length=50)
-    image = models.ImageField(upload_to=IMAGE_DIR)
+    image = models.ImageField(upload_to=IMAGE_DIR, blank=True)
     rating_points = models.IntegerField()
     rating_count = models.IntegerField()
     updated_date = models.DateTimeField(auto_now=True)
 
 class Volume(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=512)
     novel = models.ForeignKey(Novel)
     description = models.TextField()
-    image = models.ImageField(upload_to=IMAGE_DIR)
+    image = models.ImageField(upload_to=IMAGE_DIR, blank=True)
     rating_points = models.IntegerField()
     rating_count = models.IntegerField()
     
@@ -24,7 +24,7 @@ class Volume(models.Model):
         order_with_respect_to = "novel"
 
 class Chapter(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=512)
     volume = models.ForeignKey(Volume)
     rating_points = models.IntegerField()
     rating_count = models.IntegerField()
