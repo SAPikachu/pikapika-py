33d041baaef695371d173f71b55194dad92ef5ba
SAPikachu <sapikachu@gmail.com>
2012-05-12 14:11:14 +0800

    Add URL rule for serving media files

--- pikapika/urls.py
+++ pikapika/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from filebrowser.sites import site
 
@@ -17,4 +18,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
