2e5df56d7bccde0f92212ea730c172c9f3eb0c36
SAPikachu <sapikachu@gmail.com>
2012-05-14 16:26:38 +0800

    Allow users to customize layout of the uploader

--- pikapika/novel/static/modules/file-uploader/fileuploader.js
+++ pikapika/novel/static/modules/file-uploader/fileuploader.js
@@ -543,7 +543,9 @@ qq.FileUploader = function(o){
     qq.extend(this._options, o);       
 
     this._element = this._options.element;
-    this._element.innerHTML = this._options.template;        
+    if (this._options.template) {
+        this._element.innerHTML = this._options.template;        
+    }
     this._listElement = this._options.listElement || this._find(this._element, 'list');
     
     this._classes = this._options.classes;
