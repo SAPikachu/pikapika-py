46752ed877d698b6674ebd71a3d48b42c5ad916b
SAPikachu <sapikachu@gmail.com>
2012-07-18 19:19:35 +0800

    Latest novels

--- pikapika/novel/urls.py
+++ pikapika/novel/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="novel/index.html"), 
-        name="index"),
+urlpatterns = patterns('pikapika.novel.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^novel/(?P<pk>\d+)(?:/.*)?$', 
+        TemplateView.as_view(template_name="PLACEHOLDER"), 
+        name="details"),
+    url(r'^read/(?P<pk>\d+)(?:/.*)?$', 
+        TemplateView.as_view(template_name="PLACEHOLDER"), 
+        name="read"),
 )
--- pikapika/novel/views.py
+++ pikapika/novel/views.py
@@ -1,1 +1,42 @@
-# Create your views here.
+from __future__ import print_function, unicode_literals
+
+from django.shortcuts import render
+from django.db.models import Max
+
+from . import models
+
+INDEX_LATEST_NOVELS_COUNT = 15
+
+def index(request):
+    latest_novels_prep = {
+        x.latest_chapter_id: x for x in
+        models.Novel.objects.
+        order_by("-updated_date").
+        annotate(latest_chapter_id=Max("volume__chapter__id")).
+        filter(latest_chapter_id__gt=0)
+        [:INDEX_LATEST_NOVELS_COUNT]
+    }
+
+    chapters = (
+        models.Chapter.objects.
+        filter(pk__in=latest_novels_prep.keys()).
+        select_related("volume")
+    )
+
+    latest_novels = [
+        {
+            "novel": latest_novels_prep[x.pk],
+            "chapter": x,
+        }
+        for x in chapters
+    ]
+
+    latest_novels.sort(key=lambda x: x["novel"].updated_date, reverse=True)
+
+    return render(
+        request,
+        "novel/index.html",
+        {
+            "latest_novels": latest_novels,
+        },
+    )
