776811b16046f3512180db4a3b5be4e7a4b6ae05
SAPikachu <sapikachu@gmail.com>
2012-07-23 17:48:47 +0800

    live.js

---
+++ pikapika/context_processors.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, unicode_literals
+
+from django.conf import settings
+
+def misc(request):
+    return {
+        "load_live_js": settings.DEBUG and request.GET.get("live", False),
+    }
--- pikapika/novel/templatetags/html_helpers.py
+++ pikapika/novel/templatetags/html_helpers.py
@@ -7,14 +7,25 @@ register = template.Library()
 
 STATIC_VERSION = "0"
 
+def get_asset_url(name, prefix, outside):
+    is_abs = "://" in name
+    if not is_abs and not outside:
+        name = "{}/{}?{}".format(prefix, name, STATIC_VERSION)
+
+    if not is_abs:
+        name = settings.STATIC_URL + name
+
+    return name
+
 @register.simple_tag
-def css(name, media="", outside_css=False):
-    if "://" not in name and not outside_css:
-        name = "css/{}?{}".format(name, STATIC_VERSION)
-
-    name = settings.STATIC_URL + name
-
+def css(name, media="", outside=False):
     return '<link type="text/css" rel="stylesheet" href="{}" {}/>'.format(
-        name,
+        get_asset_url(name, "css", outside),
         'media="{}"'.format(media) if media else "",
+    )
+
+@register.simple_tag
+def js(name, outside=False):
+    return '<script type="text/javascript" src="{}"></script>'.format(
+        get_asset_url(name, "js", outside),
     )
