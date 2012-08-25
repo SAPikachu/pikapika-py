a41c43470772a10d97500952d20405bfb424ea21
SAPikachu <sapikachu@gmail.com>
2012-08-25 22:25:55 +0800

    Annotate hit count

--- pikapika/novel/views.py
+++ pikapika/novel/views.py
@@ -2,14 +2,17 @@ from __future__ import print_function, unicode_literals
 
 from itertools import chain
 import json
+from math import ceil
 
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Max, Sum
+from django.http import Http404
 
 from . import models, chapter_utils
 from .utils import first
 
 INDEX_LATEST_NOVELS_COUNT = 15
+NOVELS_PER_LIST_PAGE = 6
 
 def index(request):
     latest_novels_prep = {
@@ -113,3 +116,50 @@ def read(request, pk):
             "next_chapter": next_chapter,
         },
     )
+
+def list_cat(request, cat_start=None, cat_end=None, page=None):
+    if page:
+        page = int(page)
+
+    page = page or 1
+    index = page - 1
+
+    cat_start = cat_start.upper() if cat_start else None
+    cat_end = cat_end.upper() if cat_end else None
+
+    query = models.Novel.objects.all()
+    if cat_start:
+        query = (query.filter(cat_code__between=(cat_start, cat_end))
+                 if cat_end
+                 else query.filter(cat_code=cat_start))
+
+    total_novels = query.count()
+    total_pages = int(ceil(float(total_novels) / NOVELS_PER_LIST_PAGE))
+
+    if total_novels:
+        query = (
+            query.
+            annotate(hit_count=Sum("volume__chapter__hit_records__hits"))
+        )
+        query = query[index * NOVELS_PER_LIST_PAGE:
+                      (index + 1) * NOVELS_PER_LIST_PAGE]
+
+        novels = list(query)
+        if not novels:
+            raise Http404()        
+    else:
+        novels = []
+
+    return render(
+        request,
+        "novel/list.html",
+        {
+            "page": page,
+            "cat_start": cat_start,
+            "cat_end": cat_end,
+            "novels": novels,
+            "total_novels": total_novels,
+            "total_pages": total_pages,
+        },
+    )
+
