db18900953c3ba4296bec9c1c2c1b866194751d9
SAPikachu <sapikachu@gmail.com>
2012-09-18 17:38:24 +0800

    NovelManager.with_hit_count_last_week

--- pikapika/novel/managers.py
+++ pikapika/novel/managers.py
@@ -1,10 +1,15 @@
 from __future__ import print_function, unicode_literals
 
+from datetime import timedelta
+
 from django.db import models
-from django.db.models import Max, Sum
+from django.db.models import Max, Sum, Count
 from django.db.models.query import QuerySet
+from django.utils import timezone
 from generic_aggregation import generic_annotate
-from hitcount.models import HitCount
+from hitcount.models import HitCount, Hit
+
+# Note: Only import novel models inside method to prevent circular importing
 
 class NovelManager(models.Manager):
     def get_query_set(self):
@@ -12,7 +17,6 @@ class NovelManager(models.Manager):
 
 class NovelQuerySet(QuerySet):
     def with_hit_count(self):
-        # Import here to prevent circular importing
         from .models import Chapter
         return generic_annotate(
             self,
@@ -22,8 +26,23 @@ class NovelQuerySet(QuerySet):
             force_rel_model=Chapter,
         )
 
+    def with_hit_count_last_week(self):
+        from .models import Chapter
+        from django.db import connections
+        return generic_annotate(
+            self,
+            HitCount,
+            Count("volume__chapter__hitcount_object__hit__created"),
+            alias="hit_count_last_week",
+            force_rel_model=Chapter,
+            rel_slice_pos=-2,
+        ).extra(where=[Hit._meta.db_table + ".created > %s"], 
+                params=[connections[self.db].ops.value_to_db_datetime(
+                            timezone.now() - timedelta(days=7)
+                       )],
+               )
+
     def execute_with_latest_chapter(self, exclude_novel_without_chapter=True):
-        # Import here to prevent circular importing
         from .models import Chapter
 
         query = self.annotate(latest_chapter_id=Max("volume__chapter__id"))
