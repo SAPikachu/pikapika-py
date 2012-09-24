e222c1d2ba09a4c005853563c998f9eb0c9f7f7b
SAPikachu <sapikachu@gmail.com>
2012-09-24 12:01:00 +0800

    Finish search box

---
+++ pikapika/novel/static/js/pages/index.js
@@ -0,0 +1,21 @@
+require(["jquery"], function($) {
+$(function() {
+
+$(".gsc-search-box").submit(function(e) {
+    if (!$.trim($(".gsc-input").val())) {
+        e.preventDefault();
+        return false;
+    }
+});
+
+$(".icon-search-button").click(function() {
+    $(".gsc-search-box").submit();
+});
+
+$(".gsc-input").on("change keyup blur", function() {
+    var o = $(this);
+    o.toggleClass("have-text", !!o.val());
+}).change();
+
+});
+});
