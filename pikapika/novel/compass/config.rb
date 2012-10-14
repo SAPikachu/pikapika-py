5229d9be59c40c17225522d30b33f43a3d71be00
SAPikachu <sapikachu@gmail.com>
2012-10-14 18:32:46 +0800

    Enable CSS compression

--- pikapika/novel/compass/config.rb
+++ pikapika/novel/compass/config.rb
@@ -14,7 +14,7 @@ fonts_dir = "../static/font"
 http_fonts_path = ""
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
