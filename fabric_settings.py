0c9999a596ba842de7b842300e0b199227d3ee54
SAPikachu <sapikachu@gmail.com>
2012-10-17 21:31:15 +0800

    Allow pushing to live environment

--- fabric_settings.py
+++ fabric_settings.py
@@ -1,4 +1,6 @@
+from fabric.api import env as _env
+
 VIRTUALENV_ROOT = "~/virtualenv"
-PROJECT_NAME = "pikapika-saber"
+PROJECT_NAME = "pikapika" if _env.get("live", None) else "pikapika-saber"
 REMOTE_PYTHON_EXEC = "python2.7"
 MAIN_PACKAGE = "pikapika"
