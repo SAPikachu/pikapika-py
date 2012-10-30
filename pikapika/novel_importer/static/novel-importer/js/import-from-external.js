ad5741a092a12a61290b2914147495d2331cb79f
SAPikachu <sapikachu@gmail.com>
2012-10-30 19:35:44 +0800

    Different import mode

--- pikapika/novel_importer/static/novel-importer/js/import-from-external.js
+++ pikapika/novel_importer/static/novel-importer/js/import-from-external.js
@@ -16,21 +16,47 @@
         * ]
         */
         var self = this;
-        if (this.lines.length > 0) {
-            var diff = novel_importer.build_diff(content);
-            this.settings.pending_diff = diff;
-        } else {
+
+        var complete_import = function() {
+            self.settings.site_cookies = site_cookies;
+            self.save();
+            location.href = "editor";
+        };
+
+        var append_content = function() {
             $.each(content, function(i, chapter) {
-                if (i > 0) {
+                if (self.lines.length > 0) {
                     self.add_chapter_splitter();
                 }
                 $.each(chapter, function(_, paragraph) {
                     self.add_paragraph(paragraph);
                 });
             });
+            complete_import();
+        };
+
+        var merge_content = function() {
+            var diff = novel_importer.build_diff(content);
+            self.settings.pending_diff = diff;
+            complete_import();
+        };
+
+        var replace_content = function() {
+            self.clear();
+            append_content();
+        };
+
+        if (this.lines.length > 0) {
+            $("#import-mode-merge").data("handler", merge_content);
+            $("#import-mode-append").data("handler", append_content);
+            $("#import-mode-replace").data("handler", replace_content);
+            $("#do-import").click(function(e) {
+                e.preventDefault();
+                $("#mode-select input:checked").data("handler")();
+            });
+            $("#mode-select").show();
+        } else {
+            append_content();
         }
-        this.settings.site_cookies = site_cookies;
-        this.save();
-        location.href = "editor";
     };
 })(jQuery);
