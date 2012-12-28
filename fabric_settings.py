8454bdbe27868e61dc687ef55c6ef85b7402b34e
SAPikachu <sapikachu@gmail.com>
2012-12-28 12:33:33 +0800

    Print target for fabric tasks to prevent from accidentally running tasks on wrong target

--- fabric_settings.py
+++ fabric_settings.py
@@ -1,6 +1,10 @@
 from fabric.api import env as _env
 
+TARGET_LIVE = _env.get("live", None)
+
 VIRTUALENV_ROOT = "~/virtualenv"
-PROJECT_NAME = "pikapika" if _env.get("live", None) else "pikapika-saber"
+PROJECT_NAME = "pikapika" if TARGET_LIVE else "pikapika-saber"
 REMOTE_PYTHON_EXEC = "python2.7"
 MAIN_PACKAGE = "pikapika"
+
+print ("Target: " + PROJECT_NAME)
